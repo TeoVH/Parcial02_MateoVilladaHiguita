1d0a7c2c7129c509a5fab7708667be9aa1759079
TeoVH <villadamateo64@gmail.com>
2025-10-24 07:05:20 -0500

    Add Flask microservice to calculate factorial and return JSON result

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import math
+
+app = Flask(__name__)
+
+@app.route('/<int:n>', methods=['GET'])
+def servicio_factorial(n):
+    fact = math.factorial(n)
+
+    if n % 2 == 0:
+        etiqueta = "par"
+    else:
+        etiqueta = "impar"
+    
+    respuesta = { "numero": n, "factorial": fact, "etiqueta": etiqueta }
+    
+    return jsonify(respuesta), 200
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
